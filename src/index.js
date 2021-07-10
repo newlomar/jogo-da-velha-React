a31234109a6707fc118b80c4f2c14c213f7871f8
Newton Lomar <newtonlomar16@gmail.com>
2021-07-09 22:56:56 -0300

    tirando comentários

--- src/index.js
+++ src/index.js
@@ -67,21 +67,6 @@ class Board extends React.Component {
         return (
             <div>
                 {board}
-                {/* <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div> */}
             </div>
         );
     }
